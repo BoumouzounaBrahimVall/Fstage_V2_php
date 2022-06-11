040d026e4b30a136a7e0b60cdb4687b639be6985
ayoubfarid <ayoubfarid80@gmail.com>
2022-06-11 18:07:12 +0100

    add connex db

--- phpQueries/conxnBDD.php
+++ phpQueries/conxnBDD.php
@@ -3,7 +3,7 @@
 //heroku db mysql
 //mysql://b847878388960a:cec449ca@us-cdbr-east-05.cleardb.net/heroku_93aab640ed42c57?reconnect=true
 try {
-    $bdd= new PDO("mysql:host=localhost;dbname=fstage","root","");
+    //$bdd= new PDO("mysql:host=localhost;dbname=fstage","root","");
     $host = 'us-cdbr-east-05.cleardb.net';
     $dbname = 'heroku_93aab640ed42c57';
     $username = 'b847878388960a';
